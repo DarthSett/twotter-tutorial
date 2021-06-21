20bc8e5c1743b103d888f1eb886de2581126b38b
sourav <sourav@hotcocoasoftware.com>
2021-06-21 21:35:39 +0530

    First Day. learned about building single file components and using them in templates and loops and events (upto ch 4)

---
+++ src/components/TwootItem.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="twoot-item" @click="favouriteTwoot(twoot.id)">
+        <div class="user-profile__twoot">
+            <div class="twoot-item__user">
+                @{{ username }}
+            </div>
+            <div class="twoot-item__content">
+                {{ twoot.content }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TwootItem",
+        props: {
+            username: {
+                type: String,
+                required: true
+            },
+            twoot: {
+                type: Object,
+                required: true
+            }
+        },
+        methods: {
+            favouriteTwoot(id) {
+                this.$emit('favourite', id)
+            }
+        }
+    }
+</script>
+
+<style>
+    .twoot-item {
+        margin-right: 80px;
+        margin-bottom: 5px;
+        padding: 20px;              /*sets the padding from borders of div .user-profile_iser-panel*/
+        background-color: #FFFFFF;
+        border-radius: 5px;         /*rounded edge*/
+        border: 1px solid #dfe3e8;  /*sets the width,style,and color of border*/
+        box-sizing: border-box;
+        cursor: pointer;
+        transition: all 0.25s ease;
+    }
+    .twoot-item__user {
+        font-weight: bold;
+        font-size: larger;
+    }
+    .twoot-item__content {
+        font-style: italic;
+    }
+
+    .twoot-item:hover{
+        transform: scale(1.1,1.1); /*hover mechanism*/
+    }
+
+</style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="user-profile">
+        <div class="user-profile__user-panel">
+            <h1 class="user-profile__username">@{{ user.userName }}</h1> <!--References the user.userName variable from data. Double curly braces are for dynamic data points. Anything from data().return is referencable here using double curly braces-->
+            <div class="user-profile__admin-badge" v-if="user.isAdmin"> <!-- v-if stands for if else -->
+                Admin
+            </div>
+            <div class="user-profile__follower-count">
+                <strong>Followers: </strong> {{ followers }}
+            </div>
+        </div>
+        <div class="user-profile__twoots-wrapper">
+            <TwootItem
+                    v-for="twoot in user.twoots"
+                    :key="twoot.id"
+                    :username="user.userName"
+                    :twoot="twoot" @favourite="toggleFavourite"/> <!-- without :key = error  Elements in iteration expect to have 'v-bind:key' directives -->
+        </div>
+    </div>
+</template>
+
+<script>
+    import TwootItem from "./TwootItem";
+    export default {
+        name: 'UserProfile',
+        components: {TwootItem},
+        data() {
+            return {
+                followers: 0,
+                user: {
+                    id: 1,
+                    userName: "__mis2",
+                    firstName: "Sourav",
+                    lastName: "Sett",
+                    email: 'sourav241196@gmail.com',
+                    isAdmin: false,
+                    twoots: [
+                        {id: 1, content: "My first twoot! WooHoo"},
+                        {id: 2, content:"Twotter is sugoiiii!!! (❁´▽`❁)*✲ﾟ*"}
+                    ]
+                }
+            }
+        },
+        computed: {
+            fullName() {
+                return this.user.firstName + ' ' +this.user.lastName
+            }
+        },
+        methods: {
+            followUser() {
+                this.followers++
+            },
+            toggleFavourite(id) {
+                console.log(`favourited twoot with #${id}`)
+            }
+        },
+        mounted() {
+            console.log("mounted func")
+        },
+        watch: {
+            followers(newFollowerCount, oldFollowerCount) {
+                if (oldFollowerCount > newFollowerCount) {
+                    console.log("new follower")
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    .user-profile {
+        display: grid;
+        grid-template-columns: 1fr 3fr;
+        width: 100%;
+        padding: 50px 5%;
+    }
+    .user-profile__user-panel {
+        display: flex;              /*changes the layout-structure to flex style*/
+        flex-direction: column;     /*sets the flex layout-structure to be of column types*/
+        margin-right: 50px;
+        padding: 10px;              /*sets the padding from borders of div .user-profile_iser-panel*/
+        background-color: #FFFFFF;
+        border-radius: 5px;         /*rounded edge*/
+        border: 1px solid #dfe3e8;  /*sets the width,style,and color of border*/
+    }
+    h1 {
+        margin: 0;                  /*removes margin around h1*/
+    }
+
+    .user-profile__admin-badge{
+        background-color: rebeccapurple;
+        color: white;
+        border-radius: 5px;
+        margin-right: auto;
+        padding: 0 10px;
+        font-weight: bold;
+    }
+
+</style>
